598adf67aea1f22a48a8d3acdde71aeaee194fcc
Alexa Zeazas & Clayton Easterbrooks <student@epicodus.com>
2015-08-04 13:56:05 -0700

    Add second spec.

--- lib/Numbers_to_words.rb
+++ lib/Numbers_to_words.rb
@@ -1,7 +1,15 @@
 class Fixnum
   define_method(:Numbers_to_words) do
-    single_digit = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four",
-    5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine"}
+    single_digit = {0 => "zero",
+                    1 => "one",
+                    2 => "two",
+                    3 => "three",
+                    4 => "four",
+                    5 => "five",
+                    6 => "six",
+                    7 => "seven",
+                    8 => "eight",
+                    9 => "nine"}
     single_digit.fetch(self)
   end
 end
--- spec/Numbers_to_words_spec.rb
+++ spec/Numbers_to_words_spec.rb
@@ -5,4 +5,9 @@ describe('Fixnum#Numbers_to_words') do
   it("Turns single-digit numbers in numeric form into written words") do
     expect(3.Numbers_to_words()).to(eq("three"))
   end
+
+  it("Turns double-digit numbers in numeric form into written words") do
+    expect(13.Numbers_to_words()).to(eq("thirteen"))
+  end
+  
 end
